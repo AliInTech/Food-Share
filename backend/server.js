ceeb90468b207106b9dcff2ed00c8b1ce01d1993
AliInTech <laraibali1411@gmail.com>
2026-06-10 16:32:26 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,9 @@ const Food = mongoose.model('Food', new mongoose.Schema({
 // =========================
 //EMAIL SETUP
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
